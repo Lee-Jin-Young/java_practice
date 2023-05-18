b3a2d6be5db76dd0e77e96e9ea384d09f44aaaed
LeeJinyoung <dlwlsdud618@naver.com>
2023-05-18 12:40:34 +0900

    .

---
+++ Example06_SignIn/src/test/util/MyFrame.java
@@ -0,0 +1,54 @@
+package test.util;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import test.dao.MenuDao;
+import test.dto.MenuDto;
+
+public class MyFrame extends JFrame {
+	public MyFrame(String title) {
+		super(title);
+		
+		setLayout(new BorderLayout());
+		
+		JLabel label1 = new JLabel("이름");
+		JTextField inputName = new JTextField(10);
+		
+		JPanel panel = new JPanel();
+		panel.add(label1);
+		panel.add(inputName);
+		
+		add(panel,BorderLayout.NORTH);
+		
+		panel.setBackground(Color.yellow);
+		
+		JTable table = new JTable();
+		
+		String[] colNames = {"번호", "이름", "주소"};
+		//테이블에 연결할 모델객체
+		DefaultTableModel model = new DefaultTableModel(colNames, 0);
+		table.setModel(model);
+		JScrollPane scroll = new JScrollPane(table);
+		add(scroll, BorderLayout.CENTER);
+		
+		//DB에 있는 데이터를 추가
+		List<MenuDto> list = new MenuDao().getList();
+		for(MenuDto dto : list) {
+			Object[] row = {dto.getNum(), dto.getName(), dto.getAddr()};
+			model.addRow(row);
+		}
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 500);
+		setVisible(true);
+	}
+}
